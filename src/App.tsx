7f554c58162da4a6cf3ef793ca246e971580b3db
matheus souza <ruivorobado@gmail.com>
2021-09-16 09:35:45 -0300

    header criado e tema light iniciado

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import light from './styles/themes/light'
 
-function App() {
+import GlobalStyle from './styles/global'
+import { Header } from './components/Header';
+
+const App = ()=> {
   return (
-    <h2>
-      oie
-    </h2>
+    <ThemeProvider theme={light}>
+      <div className="App">
+        <GlobalStyle />
+        <Header />
+      </div>
+    </ThemeProvider>
+
   );
 }
-
 export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Container} from './styles'
+
+
+export const Header: React.FC = () =>{
+  return (
+    <Container>
+      index
+    </Container>
+  )
+}
